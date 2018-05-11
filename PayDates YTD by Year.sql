8b0fceef1bd24a0d7f5844a639f086d367745855
richardsnyder <ozgeek@gmail.com>
2018-05-11 11:17:59 +1000

    Changed to determine entire hierarchy not just parents

--- PayDates YTD by Year.sql
+++ PayDates YTD by Year.sql
@@ -1,12 +1,49 @@
-SELECT
-	DISTINCT 'Paydates YTD' Level1
-	, CASE
-	  WHEN p.RetailWeek % 2 = 0 THEN CONVERT(VARCHAR(4), p.RetailYear) + ' YTD RE/BA/YT'
-    ELSE CONVERT(VARCHAR(4), p.RetailYear) + ' YTD ME'
-    END Level2
-	, CONVERT(VARCHAR(12),p.RetailWeekEnd, 112) + ' YTD' PayDateYtd 
-FROM
-	dbo.DimDate p 
-WHERE
-	calendardate > '24-Jun-2018' 
-ORDER BY 3,2
+SELECT DISTINCT
+  'PayDates YTD' LEVEL1
+--  ,p.RetailYear
+  ,CASE 
+    WHEN p.RetailWeek % 2 = 0 
+    THEN CONVERT(VARCHAR(4),p.RetailYear) + ' YTD RE/BA/YT' 
+  END LEVEL2
+  ,CONVERT(VARCHAR(12),p.RetailWeekEnd, 112) + ' YTD' LEVEL3 
+  ,CONVERT(VARCHAR(12),Date1.PayDate, 112)LEVEL4
+FROM dbo.DimDate p 
+    INNER JOIN 
+    (
+      SELECT DISTINCT 
+      RetailYear
+      ,CONVERT(VARCHAR(12),RetailWeekEnd, 112)PayDate 
+      FROM dbo.DimDate 
+      WHERE calendardate > '30-Jun-2014' AND
+      RetailWeek % 2 = 0 
+    ) AS Date1 
+    ON Date1.PayDate <= p.RetailWeekEnd 
+    AND Date1.Retailyear = p.RetailYear
+WHERE calendardate > '30-Jun-2014' AND
+  p.RetailWeek % 2 = 0
+
+UNION ALL
+SELECT DISTINCT
+  'PayDates YTD' LEVEL1
+ -- ,p.RetailYear
+  ,CASE 
+    WHEN p.RetailWeek % 2 <> 0 
+    THEN CONVERT(VARCHAR(4),p.RetailYear) + ' YTD ME' 
+  END LEVEL2
+  ,CONVERT(VARCHAR(12),p.RetailWeekEnd, 112) + ' YTD' LEVEL3 
+  ,CONVERT(VARCHAR(12),Date1.PayDate, 112)LEVEL4
+FROM dbo.DimDate p 
+    INNER JOIN 
+    (
+      SELECT DISTINCT 
+      RetailYear
+      ,CONVERT(VARCHAR(12),RetailWeekEnd, 112)PayDate 
+      FROM dbo.DimDate 
+      WHERE calendardate > '30-Jun-2014' AND
+      RetailWeek % 2 <> 0 
+    ) AS Date1 
+    ON Date1.PayDate <= p.RetailWeekEnd 
+    AND Date1.Retailyear = p.RetailYear
+WHERE calendardate > '30-Jun-2014' AND
+  p.RetailWeek % 2 <> 0 
+ORDER BY 1,2,3
