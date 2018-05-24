7c1f498fe25b67029ce7a545f101e46dcfd18656
richards <richards@DTW00361.onepas.local>
2018-05-24 13:56:09 +1000

    Added season to query and changed filter from styleseasoncode to PatternMakerId = 17 i.e.  DWPlanned

--- Style Colour Actuals Interface.sql
+++ Style Colour Actuals Interface.sql
@@ -1,14 +1,13 @@
-
 SELECT CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) AS StyleColourID 
   ,CAST(CASE 
-    WHEN MatCustomer.BillToCustomerCode = 'DWRB' OR MatCustomer.BillToCustomerCode = 'DWRBUSD' THEN 'Rebel AU'
-    WHEN MatCustomer.BillToCustomerCode = 'DWRBZ' THEN 'Rebel NZ'
-    WHEN MatCustomer.BillToCustomerCode <> 'DWRBZ' AND MatCustomer.TerritoryCode ='NZ' THEN 'Independent NZ'
-    WHEN MatCustomer.BillToCustomerCode <> 'DWRB' OR MatCustomer.BillToCustomerCode <> 'DWRBUSD' AND MatCustomer.TerritoryCode <> 'NZ' THEN 'Independent AU'
+    WHEN MatCustomer.BillToCustomerCode = 'DWRB' OR MatCustomer.BillToCustomerCode = 'DWRBUSD' THEN 'RebelAU'
+    WHEN MatCustomer.BillToCustomerCode = 'DWRBZ' THEN 'RebelNZ'
+    WHEN MatCustomer.BillToCustomerCode <> 'DWRBZ' AND MatCustomer.TerritoryCode ='NZ' THEN 'IndependentNZ'
+    WHEN MatCustomer.BillToCustomerCode <> 'DWRB' OR MatCustomer.BillToCustomerCode <> 'DWRBUSD' AND MatCustomer.TerritoryCode <> 'NZ' THEN 'IndependentAU'
    END AS NVARCHAR(50))
   AS LocationID
   ,CAST(CAST(DimDate.RetailYear AS NVARCHAR(4)) + CAST(FORMAT(DimDate.RetailWeek,'00') AS NVARCHAR(4)) AS NVARCHAR(50)) AS TimeID
-  ,CAST('TOTAL' AS NVARCHAR(50)) AS SeasonID
+  ,CAST(MatProduct.StyleSeasonCode AS NVARCHAR(50)) AS SeasonID
   ,CAST(SUM(FactWholesaleInvoices.NetSalesExcludingTaxForeign) AS NUMERIC(16 ,2)) AS Sales_D 
   ,CAST(SUM(FactWholesaleInvoices.CostExcludingTaxForeign) AS NUMERIC(16 ,2)) AS COGS_D 
   ,CAST(SUM(FactWholesaleInvoices.Quantity) AS NUMERIC(16 ,2)) AS Sales_U
@@ -24,20 +23,21 @@ FROM DataWarehouse.dbo.FactWholesaleInvoices
     LEFT JOIN DataWarehouse.dbo.MatProduct 
     ON MatProduct.ProductId = FactWholesaleInvoices.ProductId
 WHERE 1=1
-and MatProduct.IsReported = 1
 AND MatProduct.BusinessDivisionCode = 16
-AND MatProduct.StyleSeasonCode = 'CTL'
+AND MatProduct.PatternMakerId = 17 /* DWPlanned */
 AND MatProduct.ComponentGroupCode = 'FG'
 AND MatProduct.IsActive = 1
+AND MatProduct.TunId IS NULL
+AND MatProduct.ColourId != 1
 AND DimDate.RetailWeekId >= CurrentDate.RetailWeekId -CAST(dbo.GetConfiguration('MapleLakeWeeksToActualise') AS INT)
 AND DimDate.RetailWeekId <= CurrentDate.RetailWeekId
 GROUP BY MatProduct.StyleColourCode
   ,CASE 
-    WHEN MatCustomer.BillToCustomerCode = 'DWRB' OR MatCustomer.BillToCustomerCode = 'DWRBUSD' THEN 'Rebel AU'
-    WHEN MatCustomer.BillToCustomerCode = 'DWRBZ' THEN 'Rebel NZ'
-    WHEN MatCustomer.BillToCustomerCode <> 'DWRBZ' AND MatCustomer.TerritoryCode ='NZ' THEN 'Independent NZ'
-    WHEN MatCustomer.BillToCustomerCode <> 'DWRB' OR MatCustomer.BillToCustomerCode <> 'DWRBUSD' AND MatCustomer.TerritoryCode <> 'NZ' THEN 'Independent AU'
+    WHEN MatCustomer.BillToCustomerCode = 'DWRB' OR MatCustomer.BillToCustomerCode = 'DWRBUSD' THEN 'RebelAU'
+    WHEN MatCustomer.BillToCustomerCode = 'DWRBZ' THEN 'RebelNZ'
+    WHEN MatCustomer.BillToCustomerCode <> 'DWRBZ' AND MatCustomer.TerritoryCode ='NZ' THEN 'IndependentNZ'
+    WHEN MatCustomer.BillToCustomerCode <> 'DWRB' OR MatCustomer.BillToCustomerCode <> 'DWRBUSD' AND MatCustomer.TerritoryCode <> 'NZ' THEN 'IndependentAU'
    END 
   ,CAST(CAST(DimDate.RetailYear AS NVARCHAR(4)) + CAST(FORMAT(DimDate.RetailWeek,'00') AS NVARCHAR(4)) AS NVARCHAR(50))
+  ,CAST(MatProduct.StyleSeasonCode AS NVARCHAR(50))
 ORDER BY 3,1
-
