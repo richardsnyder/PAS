315d1db2db7d124ebf5576271df1890260a097e9
RichardSnyder <ozgeek@gmail.com>
2018-05-29 12:56:36 +1000

    Finished pricing extract reduced to  current week.

--- Style Colour Pricing.sql
+++ Style Colour Pricing.sql
@@ -1,18 +1,19 @@
 SELECT DISTINCT CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) AS StyleColourId 
   ,CAST('TOTAL' AS NVARCHAR(50)) AS LocationID 
-  ,CAST(CAST(DimDate.RetailYear AS NVARCHAR(4)) + CAST(FORMAT(DimDate.RetailWeek,'00') AS NVARCHAR(4)) AS NVARCHAR(50)) AS TimeID 
+  ,CAST(FORMAT(DimDate.RetailYear,'0000') + FORMAT(DimDate.RetailWeek,'00')  AS NVARCHAR(50)) AS TimeID 
   ,CAST('TOTAL' AS NVARCHAR(50)) AS SeasonID 
-  , CAST(dbo.FuncGetTaxExclusiveAmount(COALESCE (NULLIF (WholesaleAudPrice.CurrentPrice, 0)
-  , BackupWholesaleAudPrice.CurrentPrice) ,COALESCE (NULLIF (WholesaleAudPriceScheme.TaxRate, 0) , BackupWholesaleAudPriceScheme.TaxRate)) AS DECIMAL(18 ,4)) AS Wholesale_Price 
-  ,CAST(FactProductCost.Cost / 
+  ,MAX(CAST(COALESCE(dbo.FuncGetTaxExclusiveAmount(COALESCE (NULLIF (WholesaleAudPrice.CurrentPrice, 0)
+  ,BackupWholesaleAudPrice.CurrentPrice) ,COALESCE (NULLIF (WholesaleAudPriceScheme.TaxRate, 0) , BackupWholesaleAudPriceScheme.TaxRate)),0)AS DECIMAL(18 ,4))) AS Wholesale_Price 
+  
+  ,MAX(CAST(COALESCE(FactProductCost.Cost / 
       CASE 
         WHEN MatWarehouse.CostingCurrencyId = AustralianDollarsCurrency.Id 
         THEN 1 
         ELSE CostingExchangeRate.ExchangeRate 
-      END AS DECIMAL(18,4)) AS Cost_Price 
+      END,0) AS DECIMAL(18,4))) AS Cost_Price 
 
-        , CAST(dbo.FuncGetTaxExclusiveAmount(COALESCE (NULLIF (RRPAudPrice.CurrentPrice, 0)
-  , BackupRRPAudPrice.CurrentPrice) ,COALESCE (NULLIF (RRPAudPriceScheme.TaxRate, 0) , BackupRRPAudPriceScheme.TaxRate)) AS DECIMAL(18 ,4)) AS RRP_Price 
+        , MAX(CAST(COALESCE(dbo.FuncGetTaxExclusiveAmount(COALESCE (NULLIF (RRPAudPrice.CurrentPrice, 0)
+  , BackupRRPAudPrice.CurrentPrice) ,COALESCE (NULLIF (RRPAudPriceScheme.TaxRate, 0) , BackupRRPAudPriceScheme.TaxRate)),0) AS DECIMAL(18 ,4))) AS RRP_Price 
       
       
 FROM dbo.MatProduct 
@@ -48,9 +49,7 @@ FROM dbo.MatProduct
     ON BackupRRPAudPrice.ProductId = MatProduct.ProductId 
     AND BackupRRPAudPrice.SalesPriceSchemeId = BackupRRPAudPriceScheme.Id 
     AND BackupRRPAudPrice.PriceDate = DimDate.CalendarDate
-                                              
-                                              
-                                              
+ 
     INNER JOIN DimCurrency AS AustralianDollarsCurrency 
     ON AustralianDollarsCurrency.Code = 'AUD' 
     LEFT OUTER JOIN FactExchangeRate AS CostingExchangeRate 
@@ -62,18 +61,19 @@ FROM dbo.MatProduct
     AND FactProductCost.ProductId = MatProduct.ProductId 
     AND FactProductCost.CostingZoneId = MatWarehouse.CostingZoneId 
 WHERE 1=1 
-  AND (COALESCE (NULLIF (WholesaleAudPrice.CurrentPrice, 0), NULLIF(BackupWholesaleAudPrice.CurrentPrice,0)) IS NOT NULL)
-  AND (COALESCE (NULLIF (RRPAudPrice.CurrentPrice, 0), BackupRRPAudPrice.CurrentPrice) IS NOT NULL)    
+--  AND (COALESCE (NULLIF (WholesaleAudPrice.CurrentPrice, 0), NULLIF(BackupWholesaleAudPrice.CurrentPrice,0)) IS NOT NULL)
+--  AND (COALESCE (NULLIF (RRPAudPrice.CurrentPrice, 0), BackupRRPAudPrice.CurrentPrice) IS NOT NULL)    
  AND MatWarehouse.WarehouseCode = 'DWMAIN' 
   AND MatProduct.PatternMakerId = 17 -- DWPlanned
   AND MatProduct.TunId IS NULL
-  AND MatProduct.BusinessDivisionCode IN ('18', '10', '26','16')
   AND ComponentGroupCode = 'FG'
   AND MatProduct.IsActive = 1
   AND MatProduct.ColourId != 1
-  AND DimDate.CalendarDate >= (Select distinct  RetailYearStart from DimDate where CalendarDate = CAST(GETDATE() AS DATE))
-  AND DimDate.CalendarDate <=  (Select distinct  RetailYearEnd from DimDate where CalendarDate = CAST(GETDATE() AS DATE))
-  AND DimDate.DayOfWeekId = 7 --sunday
+  AND DimDate.CalendarDate = (select max(PriceDate) from FactProductPrice)
+--  AND DimDate.DayOfWeekId = 7 --sunday
+GROUP BY CAST(MatProduct.StyleColourCode AS NVARCHAR(50))
+  ,CAST(FORMAT(DimDate.RetailYear,'0000') + FORMAT(DimDate.RetailWeek,'00')  AS NVARCHAR(50))
+
 --  
   
 ---select * from matproduct where StyleColourCode = 'DWEQ141058.DWGRY'
