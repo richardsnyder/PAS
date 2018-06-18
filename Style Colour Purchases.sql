af666d1f6c406a5f1e3a299a53521bea62535240
RichardSnyder <ozgeek@gmail.com>
2018-06-18 11:57:58 +1000

    Added warehouse AU/NZ to locationId Exclude any warehouses except for DWMain and DWNZ

--- Style Colour Purchases.sql
+++ Style Colour Purchases.sql
@@ -1,14 +1,17 @@
-SELECT CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) AS StyleColourId 
+SELECT CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) AS StyleColourId
+  ,CAST(
+    CASE 
+      WHEN MatWarehouse.WarehouseCode =  'DWMain' THEN 'WarehouseAU'
+      WHEN MatWarehouse.WarehouseCode = 'DWNZ' THEN 'WarehouseNZ'
+     END AS NVARCHAR(50)) AS LocationId
   ,CAST('WHOLESALE' AS NVARCHAR(50)) AS LocationID 
   ,CAST(CAST(DimDate.RetailYear AS NVARCHAR(4)) + CAST(FORMAT(DimDate.RetailWeek,'00') AS NVARCHAR(4)) AS NVARCHAR(50)) AS TimeID 
   ,CAST(MatProduct.StyleSeasonCode AS NVARCHAR(50)) AS SeasonID 
   ,SUM(FactPurchaseOrders.PurchaseOrderQuantity) AS PURCHASES_U
-  
   , COALESCE(SUM(CAST((FactPurchaseOrders.PurchaseOrderQuantity) * (dbo.FuncGetTaxExclusiveAmount(COALESCE (NULLIF (WholesaleAudPrice.CurrentPrice, 0) 
   , BackupWholesaleAudPrice.CurrentPrice) 
   , COALESCE (NULLIF (WholesaleAudPriceScheme.TaxRate, 0) , BackupWholesaleAudPriceScheme.TaxRate))) AS DECIMAL(18 
-  ,4))),0) AS PURCHASES_D
-   
+  ,4))),0) AS PURCHASES_D 
   , COALESCE(SUM(CAST((FactPurchaseOrders.PurchaseOrderQuantity * FactProductCost.Cost) / 
   CASE 
     WHEN MatWarehouse.CostingCurrencyId = AustralianDollarsCurrency.Id 
@@ -16,29 +19,24 @@ SELECT CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) AS StyleColourId
     ELSE CostingExchangeRate.ExchangeRate 
   END AS DECIMAL(18 
   ,4))),0) AS PURCHASES_C 
-  
 FROM dbo.FactPurchaseOrders 
     INNER JOIN MatWarehouse 
     ON FactPurchaseOrders.WarehouseId = MatWarehouse.WarehouseId 
     INNER JOIN dbo.DimDate 
     ON DimDate.CalendarDate = FactPurchaseOrders.DueDate 
     INNER JOIN dbo.DimDate CurrentDate 
-    ON CurrentDate.CalendarDate = CAST(DATEADD(DAY, -1, GETDATE()) AS DATE) 
+    ON CurrentDate.CalendarDate = CAST(GETDATE() AS DATE) 
     INNER JOIN dbo.MatProduct 
     ON MatProduct.ProductId = FactPurchaseOrders.ProductId 
-    
     LEFT OUTER JOIN DimSalesPriceScheme AS WholesaleAudPriceScheme 
     ON WholesaleAudPriceScheme.Code = 'WholesaleAUD' 
-    
     LEFT OUTER JOIN DimSalesPriceScheme AS BackupWholesaleAudPriceScheme 
     ON WholesaleAudPriceScheme.Id = WholesaleAudPriceScheme.BackupSalesPriceSchemeId 
-    
     LEFT OUTER JOIN FactProductPrice AS WholesaleAudPrice 
     ON WholesaleAudPrice.ProductId = MatProduct.ProductId 
     AND WholesaleAudPrice.SalesPriceSchemeId = WholesaleAudPriceScheme.Id 
     AND WholesaleAudPrice.PriceDate = ( SELECT MAX(PriceDate) AS Expr1 
                                         FROM FactProductPrice) 
-                                        
     LEFT OUTER JOIN FactProductPrice AS BackupWholesaleAudPrice 
     ON BackupWholesaleAudPrice.ProductId = MatProduct.ProductId 
     AND BackupWholesaleAudPrice.SalesPriceSchemeId = BackupWholesaleAudPriceScheme.Id 
@@ -60,9 +58,11 @@ WHERE MatWarehouse.BusinessDivisionCode = 16
   AND MatProduct.ComponentGroupCode = 'FG' 
   AND MatProduct.IsActive = 1 
   AND MatProduct.TunId IS NULL 
-  AND MatProduct.ColourId != 1 
+  AND MatProduct.ColourId != 1
+  AND MatWarehouse.WarehouseCode IN ('DWMAIN', 'DWNZ') 
 AND FactPurchaseOrders.SnapshotDate = CAST(DATEADD(DAY,-1,GETDATE()) AS DATE)
-GROUP BY CAST(MatProduct.StyleColourCode AS NVARCHAR(50)) 
+GROUP BY CAST(MatProduct.StyleColourCode AS NVARCHAR(50))
+  ,MatWarehouse.WarehouseCode 
   ,CAST(CAST(DimDate.RetailYear AS NVARCHAR(4)) + CAST(FORMAT(DimDate.RetailWeek,'00') AS NVARCHAR(4)) AS NVARCHAR(50)) 
   ,CAST(MatProduct.StyleSeasonCode AS NVARCHAR(50))
 ORDER BY 3
