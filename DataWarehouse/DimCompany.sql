dac4d12760e71d556c167438e5fd8a87a2b27e19
RichardSnyder <ozgeek@gmail.com>
2018-07-05 08:47:07 +1000

    Added company and date dimensions

---
+++ DataWarehouse/DimCompany.sql
@@ -0,0 +1,80 @@
+DECLARE @StagedData TABLE (
+  Chris21_SourceLevel VARCHAR(1)
+ ,Chris21_SourceCode VARCHAR(10)
+ ,Chris21_Code VARCHAR(20)
+ ,Chris21_Name VARCHAR(30)
+ ,DataWarehouseCompany_Id INT
+ ,DataWarehouseCompany_SourceKey INT
+ ,DataWarehouseCompany_Code VARCHAR(15)
+ ,DataWarehouseCompany_Name VARCHAR(40)
+ ,DataWarehouseCompany_IsActive BIT
+)
+INSERT INTO @StagedData (Chris21_SourceLevel
+, Chris21_SourceCode
+, Chris21_Code
+, Chris21_Name
+, DataWarehouseCompany_Id
+, DataWarehouseCompany_SourceKey
+, DataWarehouseCompany_Code
+, DataWarehouseCompany_Name
+, DataWarehouseCompany_IsActive)
+  /* Company */
+  SELECT DISTINCT
+    coorg.SYSOR_CODE Chris21_SourceLevel
+   ,coorg.GNA_ORG_CODE Chris21_SourceCode
+   ,coorg.GNA_ACCOUNT Chris21_Code
+   ,coorg.GNA_ORG_NAME Chris21_Name
+   ,Staging_DimLegalEntity.Id DataWarehouseCompany_Id
+   ,Staging_DimLegalEntity.SourceKey DataWarehouseCompany_SourceKey
+   ,Staging_DimLegalEntity.Code DataWarehouseCompany_Code
+   ,Staging_DimLegalEntity.Name DataWarehouseCompany_Name
+   ,Staging_DimLegalEntity.IsActive DataWarehouseCompany_IsActive
+  FROM DataWarehouseChris21RawData.dbo.Staging_EMPOS pos
+  LEFT JOIN DataWarehouseChris21RawData.dbo.Staging_ORGNA coorg
+    ON pos.POS_L1_CD = coorg.GNA_ORG_CODE
+      AND coorg.SYSOR_CODE = 1
+  LEFT JOIN DataWarehouseChris21RawData.dbo.Staging_DimLegalEntity
+    ON Staging_DimLegalEntity.Code = coorg.GNA_ACCOUNT
+  WHERE NULLIF(pos.POS_END, '0001-01-02') IS NULL
+  ORDER BY coorg.GNA_ACCOUNT
+
+MERGE DimCompany AS Destination USING @StagedData AS Source
+ON Source.Chris21_Code = Destination.Chris21_Code
+  AND Source.DataWarehouseCompany_Id = Destination.DataWarehouseCompany_Id
+
+WHEN NOT MATCHED BY TARGET
+  THEN INSERT (Chris21_SourceLevel
+    , Chris21_SourceCode
+    , Chris21_Code
+    , Chris21_Name
+    , DataWarehouseCompany_Id
+    , DataWarehouseCompany_SourceKey
+    , DataWarehouseCompany_Code
+    , DataWarehouseCompany_Name
+    , DataWarehouseCompany_IsActive
+    , UpdateDate
+    , UpdateUser
+    , CreateDate
+    , CreateUser)
+      VALUES (Source.Chris21_SourceLevel, Source.Chris21_SourceCode, Source.Chris21_Code, Source.Chris21_Name, Source.DataWarehouseCompany_Id, Source.DataWarehouseCompany_SourceKey, Source.DataWarehouseCompany_Code, Source.DataWarehouseCompany_Name, Source.DataWarehouseCompany_IsActive, NULL, NULL, current_timestamp, system_user)
+
+WHEN MATCHED
+  AND ISNULL(Source.Chris21_SourceLevel, '') != ISNULL(Destination.Chris21_SourceLevel, '')
+  OR ISNULL(Source.Chris21_SourceCode, '') != ISNULL(Destination.Chris21_SourceCode, '')
+  OR ISNULL(Source.Chris21_Code, '') != ISNULL(Destination.Chris21_Code, '')
+  OR ISNULL(Source.Chris21_Name, '') != ISNULL(Destination.Chris21_Name, '')
+  OR ISNULL(Source.DataWarehouseCompany_Id, '') != ISNULL(Destination.DataWarehouseCompany_Id, '')
+
+  THEN UPDATE
+    SET Destination.Chris21_SourceLevel = Source.Chris21_SourceLevel 
+       ,Destination.Chris21_SourceCode = Source.Chris21_SourceCode
+       ,Destination.Chris21_Code = Source.Chris21_Code
+       ,Destination.Chris21_Name = Source.Chris21_Name
+       ,Destination.DataWarehouseCompany_Id = Source.DataWarehouseCompany_Id
+       ,Destination.DataWarehouseCompany_SourceKey = Source.DataWarehouseCompany_SourceKey
+       ,Destination.DataWarehouseCompany_Code = Source.DataWarehouseCompany_Code
+       ,Destination.DataWarehouseCompany_Name = Source.DataWarehouseCompany_Name
+       ,Destination.DataWarehouseCompany_IsActive = Source.DataWarehouseCompany_IsActive
+       ,Destination.UpdateDate = current_timestamp
+       ,Destination.UpdateUser = system_user
+;
---
+++ DataWarehouse/DimDate.sql
@@ -0,0 +1,116 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT
+  CalendarDate
+ ,CalendarDayName
+ ,CalendarDateDisplayName
+ ,CalendarDateName
+ ,CalendarWeek
+ ,CalendarWeekName
+ ,CalendarWeekStart
+ ,CalendarWeekEnd
+ ,CalendarMonth
+ ,CalendarMonthName
+ ,CalendarMonthStart
+ ,CalendarMonthEnd
+ ,CalendarQuarter
+ ,CalendarQuarterName
+ ,CalendarQuarterStart
+ ,CalendarQuarterEnd
+ ,CalendarHalf
+ ,CalendarHalfName
+ ,CalendarHalfStart
+ ,CalendarHalfEnd
+ ,CalendarYear
+ ,CalendarYearName
+ ,CalendarYearStart
+ ,CalendarYearEnd
+ ,DayOfWeekId
+ ,RetailWeekId
+ ,RetailWeek
+ ,RetailWeekName
+ ,RetailWeekStart
+ ,RetailWeekEnd
+ ,RetailPeriodId
+ ,RetailPeriod
+ ,RetailPeriodName
+ ,RetailPeriodStart
+ ,RetailPeriodEnd
+ ,RetailHalf
+ ,RetailHalfName
+ ,RetailHalfStart
+ ,RetailHalfEnd
+ ,RetailSeason
+ ,RetailYearId
+ ,RetailYear
+ ,RetailYearName
+ ,RetailYearStart
+ ,RetailYearEnd
+ ,SameRetailDayPreviousYear
+ ,FinancialMonthId
+ ,FinancialMonth
+ ,FinancialMonthName
+ ,FinancialMonthStart
+ ,FinancialMonthEnd
+ ,FinancialQuarter
+ ,FinancialQuarterName
+ ,FinancialQuarterStart
+ ,FinancialQuarterEnd
+ ,FinancialHalf
+ ,FinancialHalfName
+ ,FinancialHalfStart
+ ,FinancialHalfEnd
+ ,FinancialYearId
+ ,FinancialYear
+ ,FinancialYearName
+ ,FinancialYearStart
+ ,FinancialYearEnd
+ ,RelativeCalendarWeek
+ ,RelativeRollingCalendarYear
+ ,RelativeRetailYear
+ ,RelativeRetailHalf
+ ,RelativeRetailPeriod
+ ,RelativeRetailWeek
+ ,RelativeRetailYearToDate
+ ,RelativeRetailYearToDateByWeek
+ ,RelativeRetailYearToDateByPeriod
+ ,RelativeRetailPeriodToDate
+ ,RelativeRetailWeekToDate
+ ,RelativeRetailDay
+ ,RelativeFinancialYear
+ ,RelativeFinancialHalf
+ ,RelativeFinancialQuarter
+ ,RelativeFinancialMonth
+ ,RelativeFinancialYearToGo
+ ,RelativeFinancialYearToDate
+ ,RelativeFinancialYearToDateByHalf
+ ,RelativeFinancialYearToDateByQuarter
+ ,RelativeFinancialYearToDateByMonth
+ ,RelativeFinancialMonthToGo
+ ,RelativeFinancialMonthToDate
+ ,RelativeFinancialHalfToGo
+ ,RelativeFinancialHalfToDate
+ ,RelativeFinancialQuarterToGo
+ ,RelativeFinancialQuarterToDate
+ ,RelativeFinancialDay
+ ,PreviousDay
+ ,NextDay
+ ,RelativeSeasonToDate
+ ,RelativeLast4Weeks
+ ,RelativeLast8Weeks
+ ,RelativeLast12Weeks
+ ,CASE
+    WHEN DATEPART(dw, CalendarDate) = 7 THEN CalendarDate
+    ELSE NULL
+  END PayDate
+ ,CASE
+    WHEN RetailWeek % 2 = 0 AND
+      DATEPART(dw, CalendarDate) = 7 THEN 'RE/BA/YT'
+    WHEN RetailWeek % 2 <> 0 AND
+      DATEPART(dw, CalendarDate) = 7 THEN 'ME'
+  END AS PayType
+ ,GETDATE() AS UpdateDate
+ ,SYSTEM_USER AS UpdateUser
+ ,GETDATE() AS CreateDate
+ ,SYSTEM_USER AS CreateUser
+FROM DataWarehouseChris21RawData.dbo.Staging_DimDate
+WHERE Staging_DimDate.CalendarDate >= '23-Jun-2014'
