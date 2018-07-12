b8d2fa2aab668cd278e610c43a3074e3172c69cf
RichardSnyder <ozgeek@gmail.com>
2018-07-12 12:51:43 +1000

    moved hours per financial period to DimDate

--- DataWarehouse/DimDate.sql
+++ DataWarehouse/DimDate.sql
@@ -46,7 +46,7 @@ SELECT
  ,RetailYearStart
  ,RetailYearEnd
  ,SameRetailDayPreviousYear
- ,FinancialMonthId
+ ,Staging_DimDate.FinancialMonthId
  ,FinancialMonth
  ,FinancialMonthName
  ,FinancialMonthStart
@@ -98,19 +98,35 @@ SELECT
  ,RelativeLast4Weeks
  ,RelativeLast8Weeks
  ,RelativeLast12Weeks
- ,CASE
-    WHEN DATEPART(dw, CalendarDate) = 7 THEN CalendarDate
-    ELSE NULL
+ ,CASE 
+    WHEN DATEPART(dw, CalendarDate) = 7 THEN CalendarDate ELSE NULL 
   END PayDate
- ,CASE
-    WHEN RetailWeek % 2 = 0 AND
-      DATEPART(dw, CalendarDate) = 7 THEN 'RE/BA/YT'
-    WHEN RetailWeek % 2 <> 0 AND
-      DATEPART(dw, CalendarDate) = 7 THEN 'ME'
+ ,CASE 
+    WHEN RetailWeek % 2 = 0 AND DATEPART(dw, CalendarDate) = 7 THEN 'RE/BA/YT' 
+    WHEN RetailWeek % 2 <> 0 AND DATEPART(dw, CalendarDate) = 7 THEN 'ME' 
   END AS PayType
+ ,HOURS.NumDays
+ ,HOURS.NumWeekDays
+ ,HOURS.MonthlyFullTimeHours38Hrs
+ ,HOURS.MonthlyFullTimeHours375Hrs
+ ,HOURS.RetailMonFri38Hrs
+ ,HOURS.RetailMonFri375Hrs
+ ,152 HrDashboardFortNightHours
  ,GETDATE() AS UpdateDate
- ,SYSTEM_USER AS UpdateUser
+ ,system_user AS UpdateUser
  ,GETDATE() AS CreateDate
- ,SYSTEM_USER AS CreateUser
+ ,system_user AS CreateUser
 FROM DataWarehouseChris21RawData.dbo.Staging_DimDate
+INNER JOIN (SELECT
+    FinancialMonthId
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) NumDays
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) NumWeekDays
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) * 7.6 MonthlyFullTimeHours38Hrs
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) * 7.5 MonthlyFullTimeHours375Hrs
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) * 7.6 RetailMonFri38Hrs
+   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) * 7.5 RetailMonFri375Hrs
+  FROM DataWarehouseChris21RawData.dbo.Staging_DimDate
+  GROUP BY FinancialMonthId) Hours
+  ON Hours.FinancialMonthId = Staging_DimDate.FinancialMonthId
+
 WHERE Staging_DimDate.CalendarDate >= '23-Jun-2014'
--- DataWarehouse/DimPayDate.sql
+++ DataWarehouse/DimPayDate.sql
@@ -13,19 +13,13 @@ DECLARE @Result TABLE (
  ,PayPeriodStart DATE
  ,PayPeriodEnd DATE
  ,PayType VARCHAR(10)
---,RetailYear INT
---,RetailWeek INT
---,RetailWeekId INT
---,FinancialYear INT
---,FinancialMonth INT
---,FinancialMonthId INT
 )
 
 INSERT INTO @BaseTable
   SELECT
     Staging_DimDate.CalendarDate AS PayDate
    ,Staging_DimDate.RetailWeek AS WeekNumber
-  FROM Staging_DimDate
+  FROM DataWarehouseChris21RawData.dbo.Staging_DimDate Staging_DimDate
   WHERE Staging_DimDate.CalendarDate >= '24-Jun-2014'
   AND DATEPART(dw, CalendarDate) = 7
 
@@ -50,7 +44,7 @@ BEGIN
         WHEN @WeekNumber % 2 = 0 THEN 'RE'
         ELSE 'ME'
       END PayType
-    FROM Staging_DimDate
+    FROM DataWarehouseChris21RawData.dbo.Staging_DimDate Staging_DimDate
     WHERE CalendarDate <= @KeyDate
     ORDER BY CalendarDate DESC
   FETCH NEXT FROM @MyCursor INTO @KeyDate, @WeekNumber
@@ -67,26 +61,9 @@ SELECT
  ,PayPeriodEnd
  ,DimDate.FinancialMonthId
  ,Result.PayType
- ,Hours.NumDays
- ,Hours.NumWeekDays
- ,Hours.MonthlyFullTimeHours38Hrs
- ,Hours.MonthlyFullTimeHours375Hrs
- ,Hours.RetailMonFri38Hrs
- ,Hours.RetailMonFri375Hrs
  ,152 HrDashboardFortNightHours
 INTO DataWarehouseChris21.dbo.DimPayDate
 FROM @Result Result
 INNER JOIN DataWarehouseChris21.dbo.DimDate DimDate
   ON DimDate.CalendarDate = Result.PayPeriodEnd
-INNER JOIN (SELECT
-    FinancialMonthId
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) NumDays
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) NumWeekDays
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) * 7.6 MonthlyFullTimeHours38Hrs
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 7 THEN 1 ELSE 0 END) * 7.5 MonthlyFullTimeHours375Hrs
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) * 7.6 RetailMonFri38Hrs
-   ,SUM(CASE WHEN DATEPART(dw, CalendarDate) BETWEEN 1 AND 5 THEN 1 ELSE 0 END) * 7.5 RetailMonFri375Hrs
-  FROM DataWarehouseChris21RawData.dbo.Staging_DimDate
-  GROUP BY FinancialMonthId) Hours
-  ON Hours.FinancialMonthId = DimDate.FinancialMonthId
 ORDER BY PayDateId, Result.CalendarDate
