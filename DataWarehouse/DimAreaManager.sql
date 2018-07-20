3c97802ea8bf70bf5a2369a4e7abe087f41b8e67
RichardSnyder <ozgeek@gmail.com>
2018-07-20 13:51:25 +1000

    Updated query to include name and position number

--- DataWarehouse/DimAreaManager.sql
+++ DataWarehouse/DimAreaManager.sql
@@ -1,8 +1,17 @@
+TRUNCATE TABLE DimAreaManager
 
-SELECT DISTINCT [POS_INDUSTRY]
-,[ZAM_NAME]
-FROM [dbo].[Staging_EMPOS]
-LEFT JOIN Staging_UPZAM
-ON Staging_UPZAM.ZAM_NUMBER = Staging_EMPOS.POS_INDUSTRY
+INSERT INTO dbo.DimAreaManager
+           (Chris21ProfitCentre
+           ,AreaManager
+           ,AreaManagerName
+           ,PositionNumber)
 
-
+SELECT DISTINCT pos_l5_cd Chris21ProfitCentre
+, POS_INDUSTRY AreaManager
+,Staging_UpZam.ZAM_NAME
+,Staging_UpZam.ZAM_POSOTION
+FROM FactPosition
+INNER JOIN DataWarehouseChris21RawData.dbo.Staging_UPZAM Staging_UpZam
+ON Staging_UpZam.ZAM_NUMBER = FactPosition.POS_INDUSTRY
+WHERE POS_INDUSTRY <> ''
+ORDER BY 1
