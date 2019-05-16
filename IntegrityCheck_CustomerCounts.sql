ba591ddb7b20befb6c09b015bd3528f529b1ac37
RichardSnyder <ozgeek@gmail.com>
2019-05-16 10:18:17 +1000

    Adding Jedox Batch Scripts

--- IntegrityCheck_CustomerCounts.sql
+++
@@ -1,7 +0,0 @@
-select count(distinct  retailcustomerid)
-from factretailsales
-inner join DimPerson
-on DimPerson.Id = FactRetailSales.RetailCustomerId
-where 1=1
-and DimPerson.BusinessDivisionId in (10,22)
-and FactRetailSales.SaleDate between '26-Jun-2015' and '07-May-2018'
--- README.md
+++
@@ -1,2 +0,0 @@
-# PAS
-Work at PAS
